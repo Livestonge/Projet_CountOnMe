6501eeef8a34d8129041b14f07cc22fc78dc4deb
Awaleh Hassan <awaleh@squadeasy.com>
2021-11-21 15:48:00 +0100

    Deleted file

---
+++ CountOnMeTests/CountOnMeTests.swift
@@ -0,0 +1,109 @@
+//
+//  CountOnMeTests.swift
+//  CountOnMeTests
+//
+//  Created by Awaleh Moussa Hassan on 21/11/2021.
+//  Copyright © 2021 Vincent Saluzzo. All rights reserved.
+//
+
+import XCTest
+@testable import CountOnMe
+class CountOnMeTests: XCTestCase {
+	var sut: OperationManager!
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+			try super.setUpWithError()
+			sut = OperationManager()
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+			  sut = nil
+			  try super.tearDownWithError()
+    }
+	
+	func testAdditions(){
+		
+		//given
+		sut.elements = ["2", "+", "3"]
+		
+		//when
+		try? sut.calculate()
+		
+		//then
+		XCTAssertEqual(sut.elements.last, "5")
+	}
+	
+	func testSubtraction(){
+		
+		//given
+		sut.elements = ["2", "-", "3"]
+		
+		//when
+		try? sut.calculate()
+		
+		//then
+		XCTAssertEqual(sut.elements.last, "-1")
+	}
+	
+	func testMultiplication(){
+		
+		//given
+		sut.elements = ["2", "x", "3"]
+		
+		//when
+		try? sut.calculate()
+		
+		//then
+		XCTAssertEqual(sut.elements.last, "6")
+	}
+	
+	func testDivision(){
+		
+		//given
+		sut.elements = ["12", "÷", "3"]
+		
+		//when
+		try? sut.calculate()
+		
+		//then
+		XCTAssertEqual(sut.elements.last, "4")
+	}
+	
+	func testEdgeCase_1(){
+		
+		// given
+		sut.elements = ["2", "+"]
+		
+		// when
+		try? sut.shoulAddOperand("-")
+		
+		//then
+		XCTAssertEqual(sut.elements, ["2", "+"])
+	}
+	
+	func testEdgeCase_2(){
+		
+		// given
+		sut.elements = ["2", "+", "3", "=", "5"]
+		
+		// when
+		try? sut.shoulAddOperand("+")
+		
+		//then
+		XCTAssertEqual(sut.elements, ["5", "+"])
+	}
+	
+	func testEdgeCase_3(){
+		
+		// given
+		sut.elements = ["2", "x", "5", "÷", "2", "+", "4"]
+		
+		// when
+		try? sut.calculate()
+		
+		//then
+		XCTAssertEqual(sut.elements.last, "9")
+	}
+
+}
